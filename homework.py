cef70d6ad8e55075babcf43f2903ea630f2a8761
Alexey Ageev <literzap@yandex.ru>
2023-08-24 17:55:14 +0300

    message in InfoMessage to lower case

--- homework.py
+++ homework.py
@@ -10,14 +10,14 @@ class InfoMessage:
     distance: float
     speed: float
     calories: float
-    MESSAGE: str = ('Тип тренировки: {training_type}; '
+    message: str = ('Тип тренировки: {training_type}; '
                     'Длительность: {duration:.3f} ч.; '
                     'Дистанция: {distance:.3f} км; '
                     'Ср. скорость: {speed:.3f} км/ч; '
                     'Потрачено ккал: {calories:.3f}.')
 
     def get_message(self) -> str:
-        return (self.MESSAGE).format(**asdict(self))
+        return (self.message).format(**asdict(self))
 
 
 class Training:
